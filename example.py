95abdefd769a63f2bb8f8e31a769efe38152b890
Ujjwal Subedi <ujjwal.subedi07@gmail.com>
2025-02-18 10:47:31 +0545

    Commited

---
+++ example.py
@@ -0,0 +1,19 @@
+def secure_notes():
+    label = CTkLabel(content_frame, text="Secure Notes Page", font=("Arial", 24, "bold"), text_color="black")
+    label.pack(pady=20)
+
+def credit_card():
+    label = CTkLabel(content_frame, text="Credit Card Page", font=("Arial", 24, "bold"), text_color="black")
+    label.pack(pady=20)
+
+def passwords():
+    label = CTkLabel(content_frame, text="Passwords Page", font=("Arial", 24, "bold"), text_color="black")
+    label.pack(pady=20)
+
+def email_acc():
+    label = CTkLabel(content_frame, text="Email Accounts Page", font=("Arial", 24, "bold"), text_color="black")
+    label.pack(pady=20)
+
+def bank_acc():
+    label = CTkLabel(content_frame, text="Bank Accounts Page", font=("Arial", 24, "bold"), text_color="black")
+    label.pack(pady=20)
